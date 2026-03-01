f6d11672ec81678a32d4d2ae77947605a84d7f87
Eftekhar Alam <eftialam@hotmail.com>
2026-03-01 21:21:41 +0600

    splash screen and onboard screend design

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,44 @@
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect, useState } from "react";
+import { Image, StyleSheet, View } from "react-native";
+import logo from "../assets/images/logo.png";
 import "../global.css";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
-  return <Stack />;
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      setIsReady(true);
+      await SplashScreen.hideAsync();
+    }
+    prepare();
+  }, []);
+
+  if (!isReady) {
+    return (
+      <View style={styles.container}>
+        <Image source={logo} style={styles.logo} resizeMode="contain" />
+      </View>
+    );
+  } else {
+    return <Stack screenOptions={{ headerShown: false }} />;
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logo: {
+    width: 150,
+    height: 150,
+  },
+});
---
+++ assets/data/OnboardSlider.ts
@@ -0,0 +1,31 @@
+const slider = require("../images/onboard-slider.png");
+export const sliders = [
+  {
+    id: 1,
+    title: "All your favorites",
+    description:
+      "Get all your loved foods in one once place, you just place the orer we do the rest",
+    image: slider,
+  },
+  {
+    id: 2,
+    title: "All your favorites",
+    description:
+      "Get all your loved foods in one once place, you just place the orer we do the rest",
+    image: slider,
+  },
+  {
+    id: 3,
+    title: "All your favorites",
+    description:
+      "Get all your loved foods in one once place, you just place the orer we do the rest",
+    image: slider,
+  },
+  {
+    id: 4,
+    title: "All your favorites",
+    description:
+      "Get all your loved foods in one once place, you just place the orer we do the rest",
+    image: slider,
+  },
+];
