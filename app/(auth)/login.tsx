234b2331a9b3546069f64a6da2565eb08c5ba722
Eftekhar Alam <eftialam@hotmail.com>
2026-03-03 21:23:55 +0600

    address screen design and login pertial design

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,11 +1,97 @@
-import React from "react";
-import { StyleSheet, Text } from "react-native";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { router } from "expo-router";
+import React, { useState } from "react";
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-
 const login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [checked, setChecked] = useState(false);
+
   return (
-    <SafeAreaView>
-      <Text>login</Text>
+    <SafeAreaView className="bg-black h-full">
+      <ImageBackground
+        source={require("@/assets/images/auth-bg.png")}
+        resizeMethod="cover"
+        className="flex-1 w-full h-full"
+      >
+        <View className="flex flex-col items-center justify-center mt-40">
+          <Text className="text-white text-[30px] font-bold">Log In</Text>
+          <Text className="text-gray-300 font-normal text-base">
+            Please sign in to your existing account
+          </Text>
+        </View>
+      </ImageBackground>
+      <View className="bg-white px-6 py-8 rounded-t-[30px]">
+        <View className="mb-5">
+          <Text className="mb-1 text-[14px] text-secondary">Email</Text>
+          <TextInput
+            placeholder="example@gmail.com"
+            className="bg-[#F0F5FA] text-[#A0A5BA] placeholder:text-[#A0A5BA] rounded-lg px-5 py-5"
+          />
+        </View>
+        <View className="relative mb-5">
+          <Text className="mb-1 text-[14px] text-secondary">Password</Text>
+          <TextInput
+            textContentType="password"
+            secureTextEntry={!showPassword}
+            placeholder="........"
+            className=" bg-[#F0F5FA] text-[#A0A5BA] placeholder:text-[#A0A5BA] rounded-lg px-5 py-5"
+          />
+          <TouchableOpacity
+            className="absolute right-4 top-11"
+            onPress={() => setShowPassword(!showPassword)}
+          >
+            <AntDesign
+              name={showPassword ? "eye" : "eye-invisible"}
+              size={24}
+              color="#A0A5BA"
+            />
+          </TouchableOpacity>
+        </View>
+        <View className="flex flex-row items-center justify-between mb-5">
+          <TouchableOpacity
+            className="flex-row items-center gap-2"
+            onPress={() => setChecked(!checked)}
+            activeOpacity={0.8}
+          >
+            <View
+              className={`w-6 h-6 rounded border-2 flex items-center justify-center ${
+                checked ? "bg-primary border-primary" : "border-light"
+              }`}
+            >
+              {checked && <AntDesign name="check" size={16} color="white" />}
+            </View>
+            <Text className="text-gray-700 text-base">Remember me</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => router.push("/(auth)/forgot-password")}
+          >
+            <Text className="text-primary">Forgot Password</Text>
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity className="bg-primary rounded-lg py-5 mt-2">
+          <Text className="text-white font-semibold text-xl text-center">
+            Login
+          </Text>
+        </TouchableOpacity>
+        <View className="mt-8 flex flex-row items-center gap-1 justify-center">
+          <Text className="text-base font-normal text-gray-500">
+            Don't have an account?{" "}
+          </Text>
+          <TouchableOpacity onPress={() => router.push("/(auth)/signup")}>
+            <Text className="text-primary uppercase font-semibold text-base">
+              Sign up
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
--- app/(profile)/addresses.tsx
+++ app/(profile)/addresses.tsx
@@ -1,11 +1,99 @@
+import Entypo from "@expo/vector-icons/Entypo";
+import Feather from "@expo/vector-icons/Feather";
+import { router } from "expo-router";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Addresses = () => {
+  const editAddress = () => {};
+  const deleteAddress = () => {};
   return (
-    <View>
-      <Text>Addresses</Text>
-    </View>
+    <SafeAreaView className="bg-white h-full px-5">
+      <View className="flex flex-row items-center justify-between pt-5 mb-8">
+        <View className="flex flex-row items-center gap-4">
+          <TouchableOpacity
+            onPress={() => router.back()}
+            className="bg-[#F6F8FA] rounded-full w-[45px] h-[45px] flex flex-row items-center justify-center"
+          >
+            <Entypo name="chevron-small-left" size={24} color="black" />
+          </TouchableOpacity>
+          <Text className="text-[20px] font-semibold">My Address</Text>
+        </View>
+      </View>
+
+      <View className="flex-1">
+        <ScrollView
+          contentContainerStyle={{ paddingBottom: 20 }}
+          className="pt-5"
+          showsVerticalScrollIndicator={false}
+        >
+          <View className="bg-[#F6F8FA] rounded-2xl p-5 mb-6">
+            <View className="flex flex-row items-center gap-6">
+              <View className="bg-white rounded-full p-2 items-center justify-center flex h-[45px] w-[45px]">
+                <Feather name="home" size={20} color="#369BFF" />
+              </View>
+              <View>
+                <View className="flex flex-row items-center justify-between mb-2">
+                  <Text className="text-base font-semibold  text-secondary uppercase mb-2">
+                    Home
+                  </Text>
+                  <View className="flex flex-row items-center gap-6">
+                    <TouchableOpacity onPress={editAddress}>
+                      <Feather name="edit" size={20} color="#FB6F3D" />
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={deleteAddress}>
+                      <Feather name="trash" size={20} color="#FB6F3D" />
+                    </TouchableOpacity>
+                  </View>
+                </View>
+                <Text className="text-[14px] font-normal text-[#6B6E82]">
+                  2464 Royal Ln, Meso, New Jarsey 45463
+                </Text>
+              </View>
+            </View>
+          </View>
+          <View className="bg-[#F6F8FA] rounded-2xl p-5 mb-6">
+            <View className="flex flex-row items-center gap-6 mb-4">
+              <View className="bg-white rounded-full p-2 items-center justify-center flex h-[45px] w-[45px]">
+                <Feather name="briefcase" size={20} color="#A03BB1" />
+              </View>
+              <View>
+                <View className="flex flex-row items-center justify-between mb-2">
+                  <Text className="text-base font-semibold  text-secondary uppercase mb-2">
+                    Work
+                  </Text>
+                  <View className="flex flex-row items-center gap-6">
+                    <TouchableOpacity onPress={editAddress}>
+                      <Feather name="edit" size={20} color="#FB6F3D" />
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={deleteAddress}>
+                      <Feather name="trash" size={20} color="#FB6F3D" />
+                    </TouchableOpacity>
+                  </View>
+                </View>
+                <Text className="text-[14px] font-normal text-[#6B6E82]">
+                  2464 Royal Ln, Meso, New Jarsey 45463
+                </Text>
+              </View>
+            </View>
+          </View>
+        </ScrollView>
+        <View className="mb-5 w-full">
+          <TouchableOpacity onPress={() => router.push('/(profile)/edit-addresses')} className="bg-primary w-full rounded-xl py-5 px-3">
+            <Text className="uppercase font-semibold text-white  text-center">
+              Add new address
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
